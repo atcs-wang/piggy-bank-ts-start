bda7cbe3151143860d44136e6f47b0c8134d4997
Matthew Wang <matwan@bergen.org>
2022-03-31 02:04:28 -0400

    first commit

---
+++ script.ts
@@ -0,0 +1,12 @@
+const balanceElm = document.querySelector(".balance") as HTMLElement;
+const balanceAmountElm = document.querySelector("#balance-amount") as HTMLElement;
+
+const pennyElm = document.querySelector("#penny") as HTMLElement;
+const nickelElm = document.querySelector("#nickel") as HTMLElement;
+const dimeElm = document.querySelector("#dime") as HTMLElement;
+const quarterElm = document.querySelector("#quarter") as HTMLElement;
+
+const coinAddSoundElm = document.querySelector("audio.coin-add") as HTMLAudioElement;
+const coinRemoveSoundElm = document.querySelector("audio.coin-remove") as HTMLAudioElement;
+
+const historyElm = document.querySelector(".history") as HTMLElement;
